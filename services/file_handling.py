36aba4406212be9cc12dd2487151a79c3f071047
J.P <jiangpengisme@gmail.com>
2023-06-21 15:56:44 +0800

    增加对epub图书的支持

--- services/file_handling.py
+++ services/file_handling.py
@@ -1,5 +1,9 @@
 import os
 
+import ebooklib
+from bs4 import BeautifulSoup
+from ebooklib import epub
+
 BOOK_PATH = os.environ.get("BOOK_PATH") or "book/three-body.txt"
 PAGE_SIZE = os.environ.get("PAGE_SIZE") or "500"
 
@@ -7,7 +11,8 @@ book: dict[int, str] = {}
 
 
 def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
-    seps = [',', '.', '!', ';', ':', '?', '，', '。', '！', '；', '：', '？', '《', '》', '——', '……']
+    seps = ['《', '》', '——', '……', '“', '～', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '[', ']', '【', '】', '{',
+            '}', '「', '」', '‘', '（', '）', ',', '.', '!', ';', ':', '?', '，', '。', '！', '；', '：', '？']
 
     page = text[start:start + size] if start + size <= len(text) else text[start:]
     curr_size = len(page)
@@ -22,9 +27,28 @@ def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
     return text[start:start + curr_size], curr_size
 
 
-def prepare_book(path: str) -> None:
-    with open(path, 'r') as r:
-        text = r.read()
+def prepare_book(path: str, text=None) -> None:
+    if text is None:
+        text = ''
+    if path.endswith(".txt"):
+        with open(path, 'r') as r:
+            text = r.read()
+            pages = len(text) // (int(PAGE_SIZE)) + 1
+            start = 0
+            for page_num in range(1, pages + 1):
+                page_text, page_length = _get_part_text(text, start, int(PAGE_SIZE))
+                book[page_num] = page_text.lstrip()
+                start += page_length
+    elif path.endswith(".epub"):
+        read = epub.read_epub(path)
+        chapters = []
+        for item in read.get_items():
+            if item.get_type() == ebooklib.ITEM_DOCUMENT:
+                chapters.append(item)
+        for chapter in chapters:
+            soup = BeautifulSoup(chapter.get_content(), 'html.parser')
+            text += soup.get_text()
+
         pages = len(text) // (int(PAGE_SIZE)) + 1
         start = 0
         for page_num in range(1, pages + 1):
